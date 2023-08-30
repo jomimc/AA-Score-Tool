a3858790165b10d1f4aae170b5f3bd9eb8e6dd31
Alambek Gulamidinov <alan.gulamidinov@gmail.com>
2023-08-30 18:36:57 +0900

    changed AA_Score according to the goal

--- AA_Score.py
+++ AA_Score.py
@@ -12,6 +12,9 @@ import os
 import sys
 import argparse
 
+
+#import time
+
 residue_names = [
     "HIS",
     "ASP",
@@ -52,96 +55,65 @@ def create_dict():
     return interaction_dict
 
 
-def calc_hb(hbonds, hb_dict):
-    for hb in hbonds:
-        restype = hb.restype
-        sidechain = hb.sidechain
-        energy = calc_hbond_strength(hb)
-        if restype == "HIN":
-            restype = "HIS"
-        if restype == "ACE":
-            continue
-        if sidechain:
-            key = restype + "_side"
-        else:
-            key = restype + "_main"
-        hb_dict[key] += energy
-    return
+def calc_hbonds_descriptor(protein, interactions):
+    # Indices (i,j) should be stored in interactions (PLInteraction) object
+    idx = interactions.hbond_idx
+    if not len(idx):
+        return np.nan
+    # Atomic radii (np.ndarray) should be stored in protein object
+    atomic_radii = protein.atomic_radii[idx]
+    # Sum the radii (r[i] + r[j])
+    #radii_sum = atomic_radii.sum(axis=1)
+    # Get the interatomic distances (dist_mat) from interactions object
+    dist = interactions.dist_mat[idx[:,0], idx[:,1]]
+
+    hb_energy = -(1 / (1 + np.power(dist / 2.6, 6))) / 0.58
+    return hb_energy
 
 
-def calc_hbonds_descriptor(interactions):
-    hb_dict = create_dict()
-    calc_hb(interactions.all_hbonds_ldon, hb_dict)
-    calc_hb(interactions.all_hbonds_pdon, hb_dict)
-    return hb_dict
+def calc_hydrophybic_descriptor(protein, interactions):
+    # Indices (i,j) should be stored in interactions (PLInteraction) object
+    idx = interactions.hphob_idx
+    if not len(idx):
+        return np.nan
+    # Atomic radii (np.ndarray) should be stored in protein object
+    atomic_radii = protein.atomic_radii[idx]
+    # Sum the radii (r[i] + r[j])
+    radii_sum = atomic_radii.sum(axis=1)
+    # Get the interatomic distances (dist_mat) from interactions object
+    dist = interactions.dist_mat[idx[:,0], idx[:,1]]
+
+    hphob_energy = -0.66666 * (radii_sum + 2.0 - dist)
+    hphob_energy = np.clip(hphob_energy, -1, 0)
+    return hphob_energy
 
 
-def calc_hydrophybic_descriptor(interactions):
-    hc_dict = create_dict()
-    for hc in interactions.hydrophobic_contacts:
-        restype = hc.restype
-        sidechain = hc.sidechain
-        energy = calc_hydrophobic(hc)
-        if restype[:2] == "HI" and restype not in residue_names:
-            restype = "HIS"
-        if restype == "ACE":
-            continue
-        if sidechain:
-            key = restype + "_side"
-        else:
-            key = restype + "_main"
-        hc_dict[key] += energy
-    return hc_dict
+def calc_vdw_descriptor(protein, interactions):
+    # Indices (i,j) should be stored in interactions (PLInteraction) object
+    idx = interactions.vdw_idx
+    # Atomic radii (np.ndarray) should be stored in protein object
+    atomic_radii = protein.atomic_radii[idx]
+    # Sum the radii (r[i] + r[j])
+    radii_sum = atomic_radii.sum(axis=1)
+    # Get the interatomic distances (dist_mat) from interactions object
+    dist = interactions.dist_mat[idx[:,0], idx[:,1]]
+
+    vdw_energy = np.sum(np.power((radii_sum / dist), 8) - 2 * np.power((radii_sum / dist), 4))
+    return vdw_energy
 
 
-def calc_vdw_descriptor(result, mol_lig):
-    prot = result.prot
-    residues = prot.residues
-    vdw_dict = create_dict()
-    for res in residues:
-        main_vdw, side_vdw = calc_vdw(res, mol_lig)
-        restype = res.residue_name
-        if restype[:2] == "HI" and restype not in residue_names:
-            restype = "HIS"
-        if restype not in residue_names:
-            continue
-        vdw_dict[restype + "_side"] += main_vdw
-        vdw_dict[restype + "_main"] += side_vdw
-    return vdw_dict
+def calc_ele_descriptor(protein, interactions):
+    # Indices (i,j) should be stored in interactions (PLInteraction) object
+    idx = interactions.vdw_idx
+    # Partial charges (np.ndarray) should be stored in protein object
+    partial_charge = protein.partial_charges[idx]
+    # Multiply the charges (q[i] * q[j])
+    charge_product = np.product(partial_charge, axis=1)
+    # Get the interatomic distances (dist_mat) from interactions object
+    dist = interactions.dist_mat[idx[:,0], idx[:,1]]
 
-
-def calc_ele_descriptor(result, mol_lig, mol_prot):
-    prot = result.prot
-    residues = prot.residues
-    ele_same_dict = create_dict()
-    ele_opposite_dict = create_dict()
-    for res in residues:
-        ele = Electrostatic(res, mol_lig, mol_prot)
-        restype = res.residue_name
-        if restype[:2] == "HI" and restype not in residue_names:
-            restype = "HIS"
-        if restype not in residue_names:
-            continue
-        ele_same_dict[restype + "_side"] += ele.side_ele_same
-        ele_same_dict[restype + "_main"] += ele.main_ele_same
-
-        ele_opposite_dict[restype + "_side"] += ele.side_ele_opposite
-        ele_opposite_dict[restype + "_main"] += ele.main_ele_opposite
-    return ele_same_dict, ele_opposite_dict
-
-
-def calc_desolvation_descriptor(result, mol_prot, mol_lig):
-    dehyd = Dehydration(mol_prot, mol_lig)
-    prot = result.prot
-
-    dehyd_energy = 0
-    origin = "protein"
-    for at in mol_prot.GetAtoms():
-        dehyd_energy += dehyd.calc_atom_dehyd(at, origin)
-    origin = "ligand"
-    for at in mol_lig.GetAtoms():
-        dehyd_energy += dehyd.calc_atom_dehyd(at, origin)
-    return dehyd_energy
+    ele_energy = charge_product / dist
+    return ele_energy
 
 
 def calc_metal_complexes(metal):
@@ -200,49 +172,6 @@ def calc_pication_descriptor(interactions):
     return paro_pication_energy, pic_dict
 
 
-class Model:
-    def __init__(self, arr):
-        self.arr = arr
-
-    def predict(self, data):
-        data = np.array(data)
-        return np.sum(self.arr * data)-0.999
-
-
-def load_model():
-    param = np.load("models/model-final.npy")
-    clf = Model(param)
-    return clf
-
-
-def merge_descriptors(
-        hb_dict,
-        hc_dict,
-        vdw_dict,
-        ele_same_dict,
-        ele_opposite_dict,
-        pic_dict,
-        metal_ligand,
-        tpp_energy,
-        ppp_energy,
-        ppc_energy,
-        rotat):
-    line = []
-    descriptors = [hb_dict, vdw_dict, ele_same_dict, ele_opposite_dict]
-    for des in descriptors:
-        for v in des.values():
-            line.append(v)
-    line.append(sum(hc_dict.values()))
-    line.append(sum(pic_dict.values()))
-    line.append(metal_ligand)
-    line.append(tpp_energy)
-    line.append(ppp_energy)
-    line.append(ppc_energy)
-    line.append(rotat)
-    descriptors = np.array(line)
-    return descriptors
-
-
 ### THIS IS THE MAIN FUNCTION TO RUN
 def calc_score(mol_prot):
     ### RUN THIS ONCE, AND SAVE "result" and "interactions"
@@ -250,119 +179,28 @@ def calc_score(mol_prot):
     interactions = result.interactions
 
     ### THEN OPTIMIZE THIS PART
-    hb_dict = calc_hbonds_descriptor(interactions)
-    hc_dict = calc_hydrophybic_descriptor(interactions)
-    vdw_dict = calc_vdw_descriptor(result, mol_prot)
-    ele_same_dict, ele_opposite_dict = calc_ele_descriptor(
-        result, mol_prot, mol_prot)
+    hbond_energy = calc_hbonds_descriptor(result.prot, interactions)
+    hphob_energy = calc_hydrophybic_descriptor(result.prot, interactions)
+    vdw_energy = calc_vdw_descriptor(result.prot, interactions)
+    ele_energy = calc_ele_descriptor(result.prot, interactions)
+
     metal_ligand = calc_metal_descriptor(interactions)
     tpp_energy, ppp_energy = calc_pistacking_descriptor(interactions)
     ppc_energy, pic_dict = calc_pication_descriptor(interactions)
 
-### NO NEED FOR THE REST
-#   rotat = Chem.rdMolDescriptors.CalcNumRotatableBonds(mol_lig)
-
-#   descriptors = merge_descriptors(
-#       hb_dict,
-#       hc_dict,
-#       vdw_dict,
-#       ele_same_dict,
-#       ele_opposite_dict,
-#       pic_dict,
-#       metal_ligand,
-#       tpp_energy,
-#       ppp_energy,
-#       ppc_energy,
-#       rotat)
-#   score = clf.predict(descriptors)
-#   return score
-
-def get_format(ligand_file):
-    file_format = os.path.basename( ligand_file ).split(".")[1]
-    return file_format
-
-def calc_batch(mol_prot, mol_ligs, output_file, clf):
-    for mol_lig in mol_ligs:
-        name = mol_lig.GetProp("_Name")
-        score = calc_score(mol_lig, mol_prot, clf)
-
-        if output_file:
-            with open(output_file, "a") as f:
-                f.write(name + "\t" + str(score) + "\n")
-        else:
-            print( name, score )
-    return
-
-def calc_single(mol_prot, mol_lig, output_file, clf):
-    name = mol_lig.GetProp("_Name")
-    score = calc_score(mol_lig, mol_prot, clf)
-
-    if output_file:
-        with open(output_file, "a") as f:
-            f.write(name + "\t" + str(score) + "\n")
-    else:
-        print( name, score )
-    return
-
-def calc_single(mol_prot, mol_lig, output_file, clf):
-    if not mol_lig:
-        raise RuntimeError("RDKit parse the file error")
-    name = mol_lig.GetProp("_Name")
-    score = calc_score(mol_lig, mol_prot, clf)
-
-    if output_file:
-        with open(output_file, "a") as f:
-            f.write(name + "\t" + str(score) + "\n")
-    else:
-        print( name, score )
-    return
-
-def predict_dG(mol_prot, mol_lig, output_file=None):
-    clf = load_model()
-    
-    name = mol_lig.GetProp("_Name")
-    score = calc_score(mol_lig, mol_prot, clf)
-
-    if output_file:
-        with open(output_file, "a") as f:
-            f.write(name + "\t" + str(score) + "\n")
-    else:
-        return name, score
-
-def func():
-    parser = argparse.ArgumentParser(description='parse AA Score prediction parameters')
-    parser.add_argument('--Rec', type=str, help='the file of binding pocket, only support PDB format')
-    parser.add_argument('--Lig', type=str, help='the file of ligands, support mol2, mol, sdf, PDB')
-    parser.add_argument('--Out', type=str, help='the output file for recording scores', default=None)
-    args = parser.parse_args()
-    protein_file = args.Rec
-    ligand_file = args.Lig
-    output_file = args.Out
-    
-    clf = load_model()
-    mol_prot = Chem.MolFromPDBFile(protein_file, removeHs=False)
-    lig_format = get_format(ligand_file)
-    if lig_format not in ["sdf", "mol2", "mol", "pdb"]:
-        raise RuntimeError("ligand format {} is not supported".format(lig_format))
-
-    if lig_format == "sdf":
-        mol_ligs = Chem.SDMolSupplier(ligand_file, removeHs=False)
-        calc_batch(mol_prot, mol_ligs, output_file, clf)
-    elif lig_format == "mol2":
-        mol_lig = Chem.MolFromMol2File(ligand_file, removeHs=False)
-        calc_single(mol_prot, mol_lig, output_file, clf)
-    elif lig_format == "mol":
-        mol_lig = Chem.MolFromMolFile(ligand_file, removeHs=False)
-        calc_single(mol_prot, mol_lig, output_file, clf)
-    elif lig_format == "pdb":
-        mol_lig = Chem.MolFromPDBFile(ligand_file, removeHs=False)
-        calc_single(mol_prot, mol_lig, output_file, clf)
-    return
 
 
 def run_test(protein_file, output_file=None):
+    #start_time = time.time()
+
+
     mol_prot = Chem.MolFromPDBFile(protein_file, removeHs=False)
     _ = calc_score(mol_prot)
+
+
+    # end_time = time.time()
+    # time_spend = end_time - start_time
+    # print(f"time spend: {time_spend} seconds")
 
     # if output_file:
     #     with open(output_file, "a") as f:
